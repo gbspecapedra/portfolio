9d3c95ea135c7cba6cc565ba6a787f8d29ecad6f
Gisele Pecapedra <gisele.bernardes@winedirect.com>
2024-08-21 15:41:32 -0700

    fix: navigation

--- components/FloatingNavbar.tsx
+++ components/FloatingNavbar.tsx
@@ -3,12 +3,13 @@ import React from "react";
 import { AiOutlineHome } from "react-icons/ai";
 import { GrProjects, GrUserFemale } from "react-icons/gr";
 import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
+import { Button } from "./ui";
 
 export const FloatingNavbar = () => {
   return (
     <nav className="absolute top-0 left-1/2 transform -translate-x-1/2 flex items-center justify-center bg-white/30 backdrop-blur-md shadow-xl rounded-full py-3 px-8 mt-4 space-x-8">
       <Link
-        href="undefined"
+        href="/"
         className="text-neutral-800 hover:text-accent hover:underline"
       >
         <span className="block sm:hidden">
@@ -17,33 +18,29 @@ export const FloatingNavbar = () => {
         <span className="hidden text-sm sm:block">Home</span>
       </Link>
 
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <Link href="undefined" className="text-gray-400 cursor-not-allowed">
-            <span className="block sm:hidden">
-              <GrUserFemale size={24} />
-            </span>
-            <span className="hidden text-sm sm:block">About</span>
-          </Link>
-        </TooltipTrigger>
-        <TooltipContent className="bg-white-30 backdrop-blur-md">
-          <p>coming soon</p>
-        </TooltipContent>
-      </Tooltip>
+      <Button variant="link" disabled className="m-0 p-0">
+        <Link
+          href="/about"
+          className="text-neutral-800 hover:text-accent hover:underline"
+        >
+          <span className="block sm:hidden">
+            <GrUserFemale size={24} />
+          </span>
+          <span className="hidden text-sm sm:block">About</span>
+        </Link>
+      </Button>
 
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <Link href="undefined" className="text-gray-400 cursor-not-allowed">
-            <span className="block sm:hidden">
-              <GrProjects size={22} />
-            </span>
-            <span className="hidden text-sm sm:block">Projects</span>
-          </Link>
-        </TooltipTrigger>
-        <TooltipContent className="bg-white-30 backdrop-blur-md">
-          <p>coming soon</p>
-        </TooltipContent>
-      </Tooltip>
+      <Button variant="link" disabled className="m-0 p-0">
+        <Link
+          href="/projects"
+          className="text-neutral-800 hover:text-accent hover:underline"
+        >
+          <span className="block sm:hidden">
+            <GrProjects size={22} />
+          </span>
+          <span className="hidden text-sm sm:block">Projects</span>
+        </Link>
+      </Button>
     </nav>
   );
 };
